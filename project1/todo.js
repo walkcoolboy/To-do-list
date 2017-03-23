acc4334af8982fe1ae549138f617d9582ce1616e
Li Li <lili5@aroy.ecs.vuw.ac.nz>
2017-03-23 13:52:52 +1300

    Add button to open new dialog box

--- project1/todo.js
+++ project1/todo.js
@@ -1,3 +1,7 @@
 $(document).ready(function(e) {
-  $('#add-todo').button({icons:{primary:"ui-icon-circle-plus"}})
+  $('#add-todo').button({icons:{primary:"ui-icon-circle-plus"}}).click(
+		function() {
+			$('#new-todo').dialog('open');
+		});
+  $('#new-todo').dialog({ modal : true, autoOpen : false });
 }); // end ready
