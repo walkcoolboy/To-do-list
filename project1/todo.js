3460cd1404f2866e16eb2c8ec3e792da31a70070
Li Li <walkcoolboyli@gmail.com>
2017-03-23 14:33:48 +1300

    task HTML added

--- project1/todo.js
+++ project1/todo.js
@@ -5,7 +5,19 @@ $(document).ready(function(e) {
 		});
   $('#new-todo').dialog({ modal : true, autoOpen : false,
   		buttons : {
-			"Add task" : function () { },
+			"Add task" : function () {
+				var taskName= $('#task').val();
+				if (taskName === "") {return false;}
+				var taskHTML = '<li><span class="done">%</span>';
+				taskHTML += '<span class="delete">x</span>';
+				taskHTML += '<span class="task"></span></li>';
+				var $newTask = $(taskHTML);
+				$newTask.find('.task').text(taskName);
+				$newTask.hide();
+				$('#todo-list').prepend($newTask);
+				$newTask.show('clip',250).effect('highlight',1000);
+				$(this).dialog('close');
+			},
 			"Cancel" : function () { $(this).dialog('close'); }
 		} 
 	});
